5265b036ed97a3ade2ba2e0c9813e27be4a2185a
jdufou1 <jeremy.dufourmantelle@gmail.com>
2022-04-10 18:16:27 +0200

    [PARTIE 3 ] : finition et tests

--- code/Optim.py
+++ code/Optim.py
@@ -1,96 +1,50 @@
 import numpy as np
-from Sequentiel import Sequentiel
+
+from projet_etu import Loss, Module
+
 
 class Optim():
-    """implementation de la classe Optim"""
-    def __init__(self,net,loss,eps) -> None:
-        self.net = net # resequ net 
-        self.loss = loss # fonction de cout 
-        self.eps = eps  # le pas 
-        self.error = None
-        self.sequentiel = Sequentiel(net,loss)
-        
-    def step(self, batch_x, batch_y):
-        """
-        1 etape de la descente de gradient 
-        """
-        self.sequentiel.fit(batch_x, batch_y)
-        self.error = self.sequentiel.error
-        # mise a jour backward_update_gradient
-        for i in range(len(self.net)):
 
-            if i == 0:
-                self.net[i].backward_update_gradient(batch_x, self.sequentiel.delta[-1])
-            else:
-                self.net[i].backward_update_gradient(self.sequentiel.res[i-1], self.sequentiel.delta[-i-1])
-
-            # mise a jour parametres
-            self.net[i].update_parameters(self.eps)
-            self.net[i].zero_grad()
-
-    def predict(self,xtest, biais = True):
-        if biais == True:
-            bias = np.ones((len(xtest), 1))
-            xtest = np.hstack((bias, xtest))
-
-        res_pred = []
-        res_pred.append(self.net[0].forward(xtest))
-        for i in range (1,len(self.net)):
-            res_pred.append(self.net[i].forward(res_pred[-1])) # on utilise le res du forward precedent 
-        
-        return np.argmax(res_pred[-1], axis = 1) 
-
-class SGD():
-    def __init__(self,net, loss, batch_x, batch_y) -> None:
+    def __init__(self,net : Module,loss : Loss ,eps : float) -> None:
         self.net = net
         self.loss = loss
-        self.batch_x = batch_x
-        self.batch_y = batch_y
-        self.list_error = []
+        self.eps = eps
 
-    def fit(self, taille_batch=1, n_iter= 1000, biais= True, learning_rate = 0.001):
-        if biais == True:
-            bias = np.ones((len(self.batch_x), 1))
-            self.batch_x = np.hstack((bias, self.batch_x))
+    def step(self,batch_x,batch_y) -> float :
+        """ Fonction qui calcule une passe d'apprentissage du réseau et retourne l'erreur courante"""
 
-        optim  = Optim(self.net, self.loss, learning_rate)
+        # Calcul de la passe avant forward du réseau
+        y_hat = self.net.forward(batch_x)
 
-        card = self.batch_x.shape[0]
-        nb_batchs = card//taille_batch
-        inds = []
-        for i in range(card):
-            inds.append(i)
-
-        # Création des batchs
-        np.random.shuffle(inds)
-        # print(nb_batchs)
-        # for i in range(nb_batchs):
-        #     for j in inds[i*taille_batch:(i+1)*taille_batch]:
-        #         batchs = [j]
-        batchs = [[j for j in inds[i*taille_batch:(i+1)*taille_batch]] for i in range(nb_batchs)]
-
-        for i in range(n_iter):
-            # On mélange de nouveau lorsqu'on a parcouru tous les batchs
-            if i%nb_batchs == 0:
-                np.random.shuffle(inds)
-                batchs = [[j for j in inds[i*taille_batch:(i+1)*taille_batch]] for i in range(nb_batchs)]
-
-            # Mise-à-jour sur un batch
-            batch = batchs[i%(nb_batchs)]
-            optim.step(self.batch_x[batch], self.batch_y[batch])
-            self.list_error.append(optim.error)
-            
-
-    def predict(self, xtest, biais=True):
-        """ Prédiction sur des données. Il s'agit simplement d'un forward.
-        """
-        if biais == True:
-            bias = np.ones((len(xtest), 1))
-            xtest = np.hstack((bias, xtest))
-
-        res_pred = []
-        res_pred.append(self.net[0].forward(xtest))
-        for i in range (1,len(self.net)):
-            res_pred.append(self.net[i].forward(res_pred[-1])) # on utilise le res du forward precedent 
+        # Calcul de la dérivé de la loss par rapport au y
+        last_delta = self.loss.backward(batch_y, y_hat)
         
-        return np.argmax(res_pred[-1], axis = 1)
+        # back propagation
+        delta = self.net.backward_delta(batch_x, last_delta)
+
+        # Mise a jour des gradients de chaque couche du réseau
+        self.net.backward_update_gradient(batch_x,delta)
+
+        # Mise a jour des parametres de chaque couche du réseau
+        self.net.update_parameters()
+
+        # Reinitialisation des gradients de chaque couche du réseau
+        self.net.zero_grad()
+
+        # Reinitialisation des parametres du reseau
+        self.net.initialisation_parameters()
+
+        return np.sum(self.loss.forward(batch_y, y_hat))
+
+def split_data(batch_x , batch_y , batch_size : int) :
+    number_split = len(batch_x) / batch_size
+    return np.array_split(batch_x, number_split),np.array_split(batch_y, number_split)
+
+def SGD(net : Module, loss : Loss,batch_x, batch_y , batch_size : int, nb_iter, eps : float):
+    optim = Optim(net , loss , eps)
+    array_batch_x,array_batch_y = split_data(batch_x, batch_y, batch_size)
+    list_error = []
+    for _ in range(nb_iter) : 
+        index = np.random.randint(0,(len(array_batch_x)-1))
+        list_error.append(optim.step(array_batch_x[index], array_batch_y[index]))
+    return optim.net,list_error
--- code/main_test_part3Optim.py
+++ code/main_test_part3Optim.py
@@ -1,53 +1,64 @@
+import numpy as np
+from Sequentiel import Sequentiel
 from MSELoss import MSELoss
 from Sigmoide import Sigmoide
 from TanH import Tanh
 from linear import Linear
+from Optim import Optim
+from Optim import SGD
+
 import numpy as np
 from mltools import plot_data, plot_frontiere, make_grid, gen_arti
 import matplotlib.pyplot as plt
-from Sequentiel import Sequentiel
 
-# generations de points 
+"""
+TEST OPTIM 
+"""
+
+# GENERATION DES DONNEES
 np.random.seed(1)
 X, y = gen_arti(data_type=1, epsilon=0.001) # 4 gaussiennes
 bias = np.ones((len(X), 1))
 Xbiais = np.hstack((bias, X))
 if y.ndim == 1 : 
     y = y.reshape((-1,1))
-nombre_neurone = 4
 
+# PARAMETRES DU MODELE
+eps = 1e-10
+nombre_neurone = 4
 modules = [Linear(Xbiais.shape[1],nombre_neurone),Tanh(),Linear(nombre_neurone,y.shape[1]),Sigmoide()]
 #modules = [Linear(Xbiais.shape[1],nombre_neurone),Tanh(),Linear(nombre_neurone,nombre_neurone),Tanh(),
 #Linear(nombre_neurone,nombre_neurone),Tanh(),Linear(nombre_neurone,y.shape[1]),Sigmoide()]
 loss = MSELoss()
-nn = Sequentiel(modules)
+network = Sequentiel(modules)
+optim = Optim(network , loss , eps)
+nbIter = 1000
 
-nbIter = 200
+
+# TEST POUR LA CLASSE OPTIM (a decommenter)
+"""
 list_loss = []
-
 for i in range(nbIter):
-    y_hat = nn.forward(Xbiais)
-    last_delta = loss.backward(y, y_hat)
-    delta = nn.backward_delta(Xbiais, last_delta)
-    nn.backward_update_gradient(Xbiais,delta)
-    nn.update_parameters()
-    nn.zero_grad()
-    nn.initialisation_parameters()
-    list_loss.append(np.sum(loss.forward(y, y_hat)))
-    print('Loss :',np.sum(loss.forward(y, y_hat)))
+    list_loss.append(optim.step(Xbiais,y))
+"""
 
+# TEST POUR LA FONCTION SGD
+batch_size = 500
+neural_network_trained,list_loss = SGD(network , loss , Xbiais , y , batch_size , nbIter , eps)
+
+print(Xbiais)
 # affichage de la frontiere de decision ainsi que des donnees
 plt.figure()
-plot_frontiere(X,nn.predict)
+plot_frontiere(X,neural_network_trained.predict)
 plot_data(X,y)
-plt.title(f"Frontiere de decision du module Sequence avec {nombre_neurone} neurones")
+plt.title(f"Frontiere de decision du module SGD optimisation avec {nombre_neurone} neurones")
 plt.show()
 
 # affichage erreur 
 plt.figure()
 plt.xlabel("nombre d'iteration")
 plt.ylabel("Erreur MSE")
-plt.title("Evolution de l'erreur")
+plt.title("Evolution de l'erreur SGD optimisation")
 plt.plot(np.arange(nbIter),list_loss,label="Erreur")
 plt.legend()
 plt.show()
